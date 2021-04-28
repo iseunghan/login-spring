522d9a9971ad69b9d8784094965a3d1d1446a435
iseunghan <tmdgks301@naver.com>
2021-04-28 13:35:48 +0900

    feat: MemberService 개발

---
+++ src/main/java/me/isunghan/loginspring/service/MemberService.java
@@ -0,0 +1,67 @@
+package me.isunghan.loginspring.service;
+
+import javassist.NotFoundException;
+import me.isunghan.loginspring.domain.Member;
+import me.isunghan.loginspring.domain.MemberDto;
+import me.isunghan.loginspring.repository.MemberRepository;
+import org.apache.logging.log4j.util.Strings;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    public Member findOne(Long id) throws NotFoundException {
+        return memberRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("존재하지 않는 아이디입니다."));
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public Member join(MemberDto memberDto) {
+        Optional<Member> optional = memberRepository.findMemberByUsername(memberDto.getUsername());
+        if (optional.isPresent()) {
+            throw new IllegalStateException("이미 존재하는 아이디입니다.");
+        }
+        Member member = modelMapper.map(memberDto, Member.class);
+        System.out.println(memberDto.getName() + ", " + memberDto.getUsername() + ", " + memberDto.getPassword());
+        return memberRepository.save(member);
+    }
+
+
+    public Member updateOne(Long id, MemberDto memberDto) throws NotFoundException {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 아이디입니다."));
+
+        if (Strings.isNotBlank(memberDto.getUsername())) {
+            Optional<Member> optional = memberRepository.findMemberByUsername(memberDto.getUsername());
+            if (optional.isPresent()) {
+                throw new IllegalStateException("이미 존재하는 아이디입니다.");
+            }
+            member.setUsername(memberDto.getUsername());
+        }
+        if (Strings.isNotBlank(memberDto.getPassword())) {
+            member.setPassword(memberDto.getPassword());
+        }
+        return memberRepository.save(member);
+    }
+
+    public Long deleteOne(Long id) throws NotFoundException {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 아이디입니다."));
+        memberRepository.delete(member);
+        return id;
+    }
+}
