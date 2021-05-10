46448f12ba5a8c4743c3d6d8aa81b1ab0020d028
iseunghan <tmdgks301@naver.com>
2021-05-10 13:55:13 +0900

    feat: OAuth2UserService 구현
    - CustomOAuth2UserService.java: OAuth2UserService 인터페이스를 구현하여, oauth2 인증을 완료한 user를 후처리하는 서비스 클래스

---
+++ src/main/java/me/isunghan/loginspring/security/CustomOAuth2UserService.java
@@ -0,0 +1,57 @@
+package me.isunghan.loginspring.security;
+
+import me.isunghan.loginspring.domain.Member;
+import me.isunghan.loginspring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(oAuth2UserRequest);
+
+        // 현재 진행중인 서비스를 구분하기 위해 문자열로 받음. oAuth2UserRequest.getClientRegistration().getRegistrationId()에 값이 들어있다. {registrationId='naver'} 이런식으로
+        String registrationId = oAuth2UserRequest.getClientRegistration().getRegistrationId();
+
+        // OAuth2 로그인 시 키 값이 된다. 구글은 키 값이 "sub"이고, 네이버는 "response"이고, 카카오는 "id"이다. 각각 다르므로 이렇게 따로 변수로 받아서 넣어줘야함.
+        String userNameAttributeName = oAuth2UserRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        // OAuth2 로그인을 통해 가져온 OAuth2User의 attribute를 담아주는 of 메소드.
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        // 이미 저장된 user라면, 업데이트만 시킨다.
+        Member member = saveOrUpdate(attributes, registrationId);
+        httpSession.setAttribute("member", new SessionMemer(member));
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+                , attributes.getAttributes()
+                , attributes.getNameAttributeKey());
+    }
+
+    // 혹시 이미 저장된 정보라면, update 처리
+    private Member saveOrUpdate(OAuthAttributes attributes, String registrationId) {
+        Member member = memberRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity(registrationId));
+
+        return memberRepository.save(member);
+    }
+}
